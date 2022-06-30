54b31428edd8ac89ae5e8bae22b3e2a25b78bb7a
Miraç Doğan <miracdogn@gmail.com>
2022-07-01 00:18:20 +0300

    GET,POST Methods and UI Changes

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -18,5 +18,20 @@ namespace BulkyBook.Controllers
             IEnumerable<Category> objCategoryList = _db.Categories.ToList();
             return View(objCategoryList);
         }
+
+        //GET
+        public IActionResult Create()
+        {
+            return View();
+        }
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            _db.Categories.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
